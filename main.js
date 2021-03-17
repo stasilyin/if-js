3e429e42e02decdaa5067b12700c1164572ce421
Stas Ilyin <stasilyin.by@gmail.com>
2021-03-17 19:22:13 +0300

    edit mark

--- main.js
+++ main.js
@@ -12,21 +12,21 @@ async function getDateWithRequest (url) {
       })
     })
 }
-const dataForGuestLoves = renderExam(requestUrl) ;
-  async function renderExam(url) {
-  const dataForGuestLoves = await getDateWithRequest(url);
-  dataForGuestLoves.forEach((element) => {
-    divElement.innerHTML += `
-      <figure class="guests-loves__image-wrapper guests-loves__swiper-slide">
-        <div class="guests-loves__img-wrap">
-          <img src=${element.imageUrl} alt="Hotel photo" class="guests-loves__image-photo">
-        </div>
-        <figcaption class="guests-loves__dsc-wrapper">
-          <span class="guests-loves__dsc">${element.name}</span>
-          <span class="guests-loves__dsc-city">${element.city}, ${element.country}</span>
-        </figcaption>
-      </figure>`;
-    });
+const dataForGuestLoves = renderExam(requestUrl);
+async function renderExam(url) {
+const dataForGuestLoves = await getDateWithRequest(url);
+dataForGuestLoves.forEach((element) => {
+  divElement.innerHTML += `
+    <figure class="guests-loves__image-wrapper guests-loves__swiper-slide">
+      <div class="guests-loves__img-wrap">
+        <img src=${element.imageUrl} alt="Hotel photo" class="guests-loves__image-photo">
+      </div>
+      <figcaption class="guests-loves__dsc-wrapper">
+        <span class="guests-loves__dsc">${element.name}</span>
+        <span class="guests-loves__dsc-city">${element.city}, ${element.country}</span>
+      </figcaption>
+    </figure>`;
+  });
   new Swiper('.guest-loves__swiper-container', {
     slideClass: 'guests-loves__swiper-slide',
     wrapperClass: 'guest-loves__swiper-wrapper',
@@ -47,27 +47,27 @@ const dataForGuestLoves = renderExam(requestUrl) ;
       },
       },
     });
-};
+}
 
-const formTextPeople = document.querySelector('.header-people-wrapper')
+const formTextPeople = document.querySelector('.header-people-wrapper');
 formTextPeople.innerHTML = `<div class="header-people-add">
-<div class="header-people__row">
-  <span>Adults</span> <div class = "header-people__row__button"><a class="header-people-buttons header-people-buttons__minus" id = "dellAdults">-</a><span class="header-people__row__button-value">0</span><a class="header-people-buttons header-people-buttons__plus" id = "addAdults">+</a></div>
-</div>
-<div class="header-people__row">
-  <span>Children</span><div class = "header-people__row__button"><a class="header-people-buttons header-people-buttons__minus" id = "dellChildren">-</a><span class="header-people__row__button-value" id = "value-children">0</span><a class="header-people-buttons header-people-buttons__plus" id = "addChildren">+</a></div>
-</div>
-<div class="header-people__row">
-  <span>Rooms</span><div class = "header-people__row__button"><a class="header-people-buttons header-people-buttons__minus" id = "dellRooms">-</a><span class="header-people__row__button-value">0</span><a class="header-people-buttons header-people-buttons__plus" id = "addRooms">+</a></div>
-</div>
-</div>`;
+  <div class="header-people__row">
+    <span>Adults</span> <div class = "header-people__row__button"><a class="header-people-buttons header-people-buttons__minus" id = "dellAdults">-</a><span class="header-people__row__button-value">0</span><a class="header-people-buttons header-people-buttons__plus" id = "addAdults">+</a></div>
+  </div>
+  <div class="header-people__row">
+    <span>Children</span><div class = "header-people__row__button"><a class="header-people-buttons header-people-buttons__minus" id = "dellChildren">-</a><span class="header-people__row__button-value" id = "value-children">0</span><a class="header-people-buttons header-people-buttons__plus" id = "addChildren">+</a></div>
+  </div>
+  <div class="header-people__row">
+    <span>Rooms</span><div class = "header-people__row__button"><a class="header-people-buttons header-people-buttons__minus" id = "dellRooms">-</a><span class="header-people__row__button-value">0</span><a class="header-people-buttons header-people-buttons__plus" id = "addRooms">+</a></div>
+  </div>
+  </div>`;
 
 const inputPeople = document.querySelector('#people');
 inputPeople.onfocus = function () {
   inputPeople.value = ' ';
-  const labelPeopel = document.querySelector('#header-form');
+  const labelPeople = document.querySelector('#header-form');
   formTextPeople.style.display = 'block';
-  labelPeopel.appendChild(formTextPeople);
+  labelPeople.appendChild(formTextPeople);
   changeColor();
 };
 inputPeople.onblur = function () {
@@ -77,80 +77,68 @@ inputPeople.onblur = function () {
     formTextPeople.style.display = 'none';
   }
 };
+
 const generateSelect = (event) => {
+  const selectionItemsText = `<option value = '1'>1 years old</option>
+      <option value = '2'>2 years old</option>
+      <option value = '3'>3 years old</option>
+      <option value = '4'>4 years old</option>
+      <option value = '5'>5 years old</option>
+      <option value = '6'>6 years old</option>
+      <option value = '7'>7 years old</option>
+      <option value = '8'>8 years old</option>
+      <option value = '9'>9 years old</option>
+      <option value = '10'>10 years old</option>
+      <option value = '11'>11 years old</option>
+      <option value = '12'>12 years old</option>
+      <option value = '13'>13 years old</option>
+      <option value = '14'>14 years old</option>
+      <option value = '15'>15 years old</option>
+      <option value = '16'>16 years old</option>
+      <option value = '17'>17 years old</option>
+      </select>`;
   const selectionChildren = document.createElement('div');
   selectionChildren.classList.add('header-people__info-children');
   selectionChildren.innerHTML = `<span>What is the age of the child you’re travelling with?</span>`;
   const selectionItems = document.createElement('select');
-  selectionItems.classList.add('header-people__select-yers-children');
-  selectionItems.innerHTML = `<option value = '1'>1 yers old</option>
-  <option value = '2'>2 yers old</option>
-  <option value = '3'>3 yers old</option>
-  <option value = '4'>4 yers old</option>
-  <option value = '5'>5 yers old</option>
-  <option value = '6'>6 yers old</option>
-  <option value = '7'>7 yers old</option>
-  <option value = '8'>8 yers old</option>
-  <option value = '9'>9 yers old</option>
-  <option value = '10'>10 yers old</option>
-  <option value = '11'>11 yers old</option>
-  <option value = '12'>12 yers old</option>
-  <option value = '13'>13 yers old</option>
-  <option value = '14'>14 yers old</option>
-  <option value = '15'>15 yers old</option>
-  <option value = '16'>16 yers old</option>
-  <option value = '17'>17 yers old</option>
-  </select>`;
+  selectionItems.classList.add('header-people__select-years-children');
+  selectionItems.innerHTML = selectionItemsText;
   let generateWhile = 0;
-  event.target.id == 'dellChildren' ?
+  event.target.id === 'dellChildren' ?
   generateWhile = event.target.nextSibling.innerHTML : generateWhile = event.target.previousSibling.innerHTML;
   if (document.querySelector('.header-people__info-children')) {
     document.querySelector('.header-people__info-children').remove();
   }
   for (let i = 0; i < generateWhile; i++) {
-    if (i == 0) {
+    if (i === 0) {
       document.querySelector('.header-people-add').appendChild(selectionChildren);
       selectionChildren.appendChild(selectionItems);
     } else {
       const selectionItems = document.createElement('select');
-      selectionItems.classList.add('header-people__select-yers-children');
-      selectionItems.innerHTML = `<option value = '1'>1 yers old</option>
-        <option value = '2'>2 yers old</option>
-        <option value = '3'>3 yers old</option>
-        <option value = '4'>4 yers old</option>
-        <option value = '5'>5 yers old</option>
-        <option value = '6'>6 yers old</option>
-        <option value = '7'>7 yers old</option>
-        <option value = '8'>8 yers old</option>
-        <option value = '9'>9 yers old</option>
-        <option value = '10'>10 yers old</option>
-        <option value = '11'>11 yers old</option>
-        <option value = '12'>12 yers old</option>
-        <option value = '13'>13 yers old</option>
-        <option value = '14'>14 yers old</option>
-        <option value = '15'>15 yers old</option>
-        <option value = '16'>16 yers old</option>
-        <option value = '17'>17 yers old</option>
-        </select>`;
+      
+      selectionItems.classList.add('header-people__select-years-children');
+      selectionItems.innerHTML = selectionItemsText;
       selectionChildren.appendChild(selectionItems);
     }
   }
 };
 
 const changeColor = () => document.querySelectorAll('.header-people__row__button-value').forEach(element => {
-  if (element.innerHTML == 0) {
-    element.previousSibling.style.borderColor = '#CECECE';
-    element.previousSibling.style.color = '#CECECE';
+  const colorLightTelegrey = '#CECECE';
+  const colorLightDenim = '#3077C6';
+  if (+element.innerHTML === 0) {
+    element.previousSibling.style.borderColor = colorLightTelegrey;
+    element.previousSibling.style.color = colorLightTelegrey;
   } else {
-    element.previousSibling.style.borderColor = '#3077C6';
-    element.previousSibling.style.color = '#3077C6';
+    element.previousSibling.style.borderColor = colorLightDenim;
+    element.previousSibling.style.color = colorLightDenim;
   }
-  if (element.innerHTML == 30 || (element.innerHTML == 10 && element.id == 'value-children')) {
-    element.nextSibling.style.borderColor = '#CECECE';
-    element.nextSibling.style.color = '#CECECE';
+  if (+element.innerHTML === 30 || (+element.innerHTML === 10 && element.id === 'value-children')) {
+    element.nextSibling.style.borderColor = colorLightTelegrey;
+    element.nextSibling.style.color = colorLightTelegrey;
   } else {
-    element.nextSibling.style.borderColor = '#3077C6';
-    element.nextSibling.style.color = '#3077C6';
+    element.nextSibling.style.borderColor = colorLightDenim;
+    element.nextSibling.style.color = colorLightDenim;
   }
 });
 const generateValueInputPeople = () => {
@@ -166,21 +154,25 @@ const generateValueInputPeople = () => {
       case '0': finalValue += result[key] + ' Adults - '; break;
       case '1': finalValue += result[key] + ' Children - '; break;
       case '2': finalValue += result[key] + ' Rooms'; break;
-    }
+    };
   }
   inputPeopleInfo.value = finalValue;
 };
 const addChildren = (event) => {
   event.stopPropagation();
   event.preventDefault();
-  let value = event.target.previousSibling.innerHTML;
-  if ((event.target.id == 'addAdults' || event.target.id == 'addRooms') && (value < 30 && value >= 0)) {
+  const idAddAdultsBtn = 'addAdults';
+  const idAddRoomsBtn = 'addRooms';
+  const idAddChildrenBtn = 'addChildren';
+  let value = +event.target.previousSibling.innerHTML;
+  console.log(value)
+  if ((event.target.id === idAddAdultsBtn || event.target.id === idAddRoomsBtn) && (value < 30 && value >= 0)) {
     event.target.previousSibling.innerHTML = ++value;
   }
-  if (event.target.id == 'addChildren' && value == 0) {
+  if (event.target.id === idAddChildrenBtn && value === 0) {
     event.target.previousSibling.innerHTML = ++value;
     generateSelect(event);
-  } else if (event.target.id == 'addChildren' && (value >= 1 && value < 10)) {
+  } else if (event.target.id === idAddChildrenBtn && (value >= 1 && value < 10)) {
     event.target.previousSibling.innerHTML = ++value;
     generateSelect(event);
   }
@@ -191,10 +183,14 @@ const dellChildren = (event) => {
   event.stopPropagation();
   event.preventDefault();
   let value = event.target.nextSibling.innerHTML;
-  if ((event.target.id == 'dellAdults' || event.target.id == 'dellRooms') && (value > 0)) {
+  const idDellAdultsBtn = 'dellAdults';
+  const idDellRoomsBtn = 'dellRooms';
+  const idDellChildrenBtn = 'dellChildren';
+  
+  if ((event.target.id === idDellAdultsBtn || event.target.id === idDellRoomsBtn) && (value > 0)) {
     event.target.nextSibling.innerHTML = --value;
   }
-  if (event.target.id == 'dellChildren' && value >= 1) {
+  if (event.target.id === idDellChildrenBtn && value >= 1) {
     event.target.nextSibling.innerHTML = --value;
     generateSelect(event);
   }
